6b7a588910d209458efd0cf1f88ec2e228c123f6
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-15 21:44:43 +0530

    Added Movie Adater

--- app/src/main/java/com/example/moviespot/MovieAdapter.java
+++ app/src/main/java/com/example/moviespot/MovieAdapter.java
@@ -4,16 +4,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
-
-    private ArrayList<Movie> movies;
-    private MovieAdapterOnClickHandler mClickHandler;
-
-    public MovieAdapter(MovieAdapterOnClickHandler clickHandler){
-        this.mClickHandler = clickHandler;
-    }
-    public interface MovieAdapterOnClickHandler{
-        void onClick(Movie movie);
-    }
-
-}
+//public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+//
+//    private ArrayList<Movie> movies;
+//    private MovieAdapterOnClickHandler mClickHandler;
+//
+//    public MovieAdapter(MovieAdapterOnClickHandler clickHandler){
+//        this.mClickHandler = clickHandler;
+//    }
+//    public interface MovieAdapterOnClickHandler{
+//        void onClick(Movie movie);
+//    }
+//
+//}
