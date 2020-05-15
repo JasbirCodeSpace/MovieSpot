7c36cdd8af0f4f664a1081b9f00855f934fd74ec
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-15 21:43:44 +0530

    Updated MainActivity

--- app/src/main/java/com/example/moviespot/MainActivity.java
+++ app/src/main/java/com/example/moviespot/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.moviespot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private ArrayList<Movie> popularMovies;
+    private ArrayList<Movie> topRatedMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgressBar = (ProgressBar)findViewById(R.id.movies_progress_bar);
+    }
+
+    public class FetchMovies extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressBar.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            popularMovies = new ArrayList<>();
+            topRatedMovies = new ArrayList<>();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
     }
 }
