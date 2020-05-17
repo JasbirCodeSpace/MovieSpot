089cff784321b8cae5087d8e9917c927fb99d6ce
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-17 19:43:58 +0530

    Updated MovieAdapter class

--- app/src/main/java/com/example/moviespot/MovieAdapter/MovieAdapter.java
+++ app/src/main/java/com/example/moviespot/MovieAdapter/MovieAdapter.java
@@ -1,16 +1,21 @@
 package com.example.moviespot.MovieAdapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.moviespot.MovieModel.Movie;
+import com.example.moviespot.R;
 import com.example.moviespot.Utils.OnItemClickListener;
 
 import java.util.List;
 
-public class MovieAdapter extends RecyclerView.Adapter{
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
     List<Movie> movies;
     OnItemClickListener clickListener;
@@ -21,17 +26,42 @@ public class MovieAdapter extends RecyclerView.Adapter{
     }
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.movie_item,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.onBind(movies.get(position),clickListener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView poster;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            poster = itemView.findViewById(R.id.iv_movie);
+        }
+        public void onBind(final Movie movie, final OnItemClickListener listener){
+            Glide.with(itemView.getContext())
+                    .load(posterPath(movie.getmPoster()))
+                    .into(poster);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(movie);
+                }
+            });
+        }
+    }
+
+    private static String posterPath(String res){
+        return "https://image.tmdb.org/t/p/" + "w500" +res;
     }
 }
